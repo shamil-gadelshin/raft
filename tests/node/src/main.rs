06bdfcbce0b8f4762af0d15c013de1651369b9f3
Shamil Gadelshin <shamilgadelshin@gmail.com>
2019-10-02 16:27:32 +0300

    Modify ‘Single node example project’
    Modify ‘Single node example project’ - to support network peer communicator.

--- tests/node/src/main.rs
+++ tests/node/src/main.rs
@@ -5,6 +5,7 @@ extern crate crossbeam_channel;
 
 use std::time::Duration;
 use std::io::Write;
+use std::collections::HashMap;
 
 use chrono::prelude::{DateTime, Local};
 
@@ -15,8 +16,8 @@ use raft::{NodeState, NodeLimits};
 use raft::NodeConfiguration;
 
 use raft_modules::{MemoryRsm, RandomizedElectionTimer, MockNodeStateSaver, ClusterConfiguration};
+use raft_modules::{NetworkPeerCommunicator, StaticTableServiceDiscovery};
 use raft_modules::MemoryOperationLog;
-use raft_modules::{InProcPeerCommunicator};
 use raft_modules::NetworkClientCommunicator;
 
 
@@ -39,8 +40,9 @@ fn main() {
     info!("Server started");
     let all_nodes = vec![node_id];
     let cluster_configuration = ClusterConfiguration::new(all_nodes.clone());
-    let communication_timeout = Duration::from_millis(500);
-    let client_request_handler = NetworkClientCommunicator::new(get_address(node_id), node_id, communication_timeout, true);
+
+    let client_request_handler = NetworkClientCommunicator::new(get_client_requests_address(node_id), node_id, get_communication_timeout(), true);
+    let peer_request_handler = get_network_peer_communicator(node_id);
 
     let node_config = NodeConfiguration {
         node_state: NodeState {
@@ -49,7 +51,7 @@ fn main() {
             vote_for_id: None
         },
         cluster_configuration: cluster_configuration.clone(),
-        peer_communicator: InProcPeerCommunicator::new(all_nodes.clone(), communication_timeout),
+        peer_communicator: peer_request_handler,
         client_communicator: client_request_handler.clone(),
         election_timer: RandomizedElectionTimer::new(1000, 4000),
         limits: NodeLimits::default(),
@@ -67,8 +69,30 @@ fn main() {
     }
 }
 
+fn get_communication_timeout() -> Duration{
+    Duration::from_millis(500)
+}
 
-fn get_address(node_id : u64) -> String{
+
+fn get_client_requests_address(node_id : u64) -> String{
     format!("127.0.0.1:{}", 50000 + node_id)
 }
 
+fn get_network_peer_communicator(node_id: u64) -> NetworkPeerCommunicator<StaticTableServiceDiscovery>{
+    let table : HashMap<u64, String>  = [
+        (1, get_peer_requests_address(1)),
+        (2, get_peer_requests_address(2)),
+        (3, get_peer_requests_address(3))]
+        .iter().cloned().collect();
+
+    let service_discovery = StaticTableServiceDiscovery::new(table);
+
+    let host = get_peer_requests_address(node_id);
+
+    NetworkPeerCommunicator::new(host, node_id,get_communication_timeout(), true, service_discovery)
+}
+
+
+pub fn get_peer_requests_address(node_id : u64) -> String{
+    format!("127.0.0.1:{}", 60000 + node_id)
+}
